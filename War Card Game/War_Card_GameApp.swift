32c86e10611794a417827be60b93a6b836af39c0
Sebastian Krzysiak <102769429+sebastiankrzysiak@users.noreply.github.com>
2023-05-22 17:56:54 -0500

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Sebastian Krzysiak on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
